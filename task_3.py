30e3cd33d3af047825adf4a185bdaa264c49c450
Maryia Yermakovich <maryia.yermakovich@pandadoc.com>
2022-08-16 15:26:46 +0200

    added the user input handling

--- task_3.py
+++ task_3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import re
+
 class MyCripto:
   def __init__(self, shift: int):
     if shift > 26 or shift < -26:
@@ -71,12 +73,47 @@ class MyCripto:
     else:
       return my_string
 
-shifts = [0, 3, 33, 145, -7, -40]
-for shift in shifts:
-  my_test = MyCripto(shift)
-  print("Shift: " , shift)
-  print(my_test.encrypt("cat"))
-  print(my_test.decrypt("fdw"))
-  print(my_test.encrypt("кот"))
-  print(my_test.encrypt("Axolotl"))
-  print(my_test.decrypt("Darorwo"))
+# testcases
+#shifts = [0, 3, 33, 145, -7, -40]
+#for shift in shifts:
+#  my_test = MyCripto(shift)
+#  print("Shift: " , shift)
+#  print(my_test.encrypt("cat"))
+#  print(my_test.decrypt("fdw"))
+#  print(my_test.encrypt("кот"))
+#  print(my_test.encrypt("Axolotl"))
+#  print(my_test.decrypt("Darorwo"))
+
+# main logic
+welcome_string = "Welcome to MyCripto. Options:\n- \
+  provide an integer to serve as a basis for encoding/decoding\n \
+  - enter `encrypt ` followed by your string to encode it\n \
+  - enter `decrypt ` followed by your string to decode it\n \
+  - enter `exit` to stop the app"
+
+
+user_input = input(welcome_string).strip()
+
+while user_input != 'exit':
+  if 'encrypt' in user_input:
+    m = re.search(r'^(encrypt) (.+)$', user_input)
+    my_word = m.group(2)
+    if shift:
+      print(MyCripto(shift).encrypt(my_word))
+      user_input = input(welcome_string)
+    else:
+      user_input = input(welcome_string)
+  elif 'decrypt' in user_input:
+    m = re.search(r'^(decrypt) (.+)$', user_input)
+    my_word = m.group(2)
+    if shift:
+      print(MyCripto(shift).decrypt(my_word))
+      user_input = input(welcome_string)
+    else:
+      user_input = input(welcome_string)
+  elif int(user_input):
+    shift = int(user_input)
+    print("Successfully set the shift key: ", shift)
+    user_input = input(welcome_string)
+  else:
+    user_input = input(welcome_string)
